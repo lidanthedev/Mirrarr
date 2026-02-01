e2dd183b953cbddc92bf0fec100e450da29238c0
LidanTheDev <ls301205@gmail.com>
2026-02-01 23:29:09 +0200

    added the api request for getting video providers

---
+++ rivestream/video_providers.js
@@ -0,0 +1,13 @@
+fetch("https://rivestream.org/api/backendfetch?requestID=VideoProviderServices&secretKey=rive&proxyMode=noProxy", {
+  "headers": {
+    "accept": "application/json",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Brave\";v=\"144\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\""
+  },
+  "referrer": "https://rivestream.org/watch?type=tv&id=1418&season=1&episode=1",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
+});
