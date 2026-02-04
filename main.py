b84f4cc4c78059a7ba4bd46ca94a10334256e212
LidanTheDev <ls301205@gmail.com>
2026-02-05 01:12:07 +0200

    refactor API request parameters for video provider

--- main.py
+++ main.py
@@ -20,10 +20,19 @@ def main():
 
     session = niquests.Session()
 
-    burp0_url = "https://rivestream.org:443/api/backendfetch?requestID=tvVideoProvider&id=1418&season=1&episode=1&service=flowcast&secretKey=NGFmNjhjZDg=&proxyMode=noProxy"
+    burp0_base_url = "https://rivestream.org:443/api/backendfetch"
+    burp0_params = {
+        "requestID": "tvVideoProvider",
+        "id": "1418",
+        "season": "1",
+        "episode": "1",
+        "service": "flowcast",
+        "secretKey": "NGFmNjhjZDg=",
+        "proxyMode": "noProxy",
+    }
     # burp0_cookies = {"_ga": "GA1.1.753096691.1769890818", "_ga_TY1B74WN3B": "GS2.1.s1769890818$o1$g0$t1769890855$j23$l0$h0"}
     # burp0_headers = {"Sec-Ch-Ua-Platform": "\"Linux\"", "Accept-Language": "en-GB,en;q=0.9", "Accept": "application/json", "Sec-Ch-Ua": "\"Not_A Brand\";v=\"99\", \"Chromium\";v=\"142\"", "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36", "Sec-Ch-Ua-Mobile": "?0", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Referer": "https://rivestream.org/watch?type=tv&id=1418&season=1&episode=1", "Accept-Encoding": "gzip, deflate, br", "Priority": "u=1, i"}
-    r = session.get(burp0_url)
+    r = session.get(burp0_base_url, params=burp0_params)
     print(r.json())
 
 
