0c92ad4794a6be158a97a5a3afb8627cb95ebdbc
LidanTheDev <ls301205@gmail.com>
2026-02-11 23:38:32 +0200

    Checking for wrong permission before changing it Fixed gosu to runuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,23 @@ set -e
 
 # Change ownership of the mounted volumes to the mirarr user
 # This happens at runtime, so it fixes the host-mount issue
-chown -R mirarr:mirarr /app/data /app/downloads
+fix_permissions() {
+    local dir="$1"
+    
+    if [ -d "$dir" ]; then
+        # Check ownership of the directory itself
+        if [ "$(stat -c '%U' "$dir")" != "mirarr" ]; then
+            echo "Fixing permissions for $dir..."
+            chown -R mirarr:mirarr "$dir" || true
+        else
+            echo "Permissions for $dir are correct. Skipping recursive chown."
+        fi
+    fi
+}
+
+fix_permissions "/app/data"
+fix_permissions "/app/downloads"
 
 # Execute the CMD passed from Dockerfile/Compose
-# We use gosu to run the app as the 'mirarr' user
+# We use runuser to run the app as the 'mirarr' user
 exec runuser -u mirarr -- "$@"
